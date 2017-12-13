c80aa881f99d838b8aa38cb9d18c0000dde3e6b2
eric trotchie <Eric-JT@users.noreply.github.com>
2017-12-12 19:23:49 -0800

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const GULP         = require("gulp");
+const PREFIX       = require("gulp-autoprefixer");
+
+
+/**
+ * Compile scss files
+ * save css build in theme folder
+ */
+GULP.task("prefix", function () {
+  return GULP.src("style.css")
+  .pipe(PREFIX(["last 15 versions", "> 1%", "ie 8", "ie 7"], { cascade: true }))
+  .pipe(GULP.dest('./'));
+});
+
+
+/*
+* Watch folders for changes
+*/
+GULP.task("watch", function() {
+  GULP.watch("style.css", ["prefix"]);
+});
+
+
+GULP.task("default", ["watch"]);
